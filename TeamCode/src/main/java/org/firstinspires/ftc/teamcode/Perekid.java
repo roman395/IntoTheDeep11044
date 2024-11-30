2be7189dd5df13d117360746e9bee1d8afbc99ce
roman395 <goruskinroma@gmail.com>
2024-11-30 15:58:28 +0300

    first commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Perekid.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Perekid.java
@@ -2,13 +2,15 @@ package org.firstinspires.ftc.teamcode;
 
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.ServoImplEx;
 
 public class Perekid{
-    ServoImplEx s1;
-    ServoImplEx s2;
+    CRServo s1;
+    CRServo s2;
 
     LinearOpMode linearOpMode;
     HardwareMap hardwareMap;
@@ -17,23 +19,17 @@ public class Perekid{
         this.linearOpMode=linearOpMode;
         hardwareMap=linearOpMode.hardwareMap;
 
-        s1=hardwareMap.get(ServoImplEx.class,"leftP");
-        s2=hardwareMap.get(ServoImplEx.class,"rightP");
+        s1=hardwareMap.get(CRServo.class,"leftP");
+        s2=hardwareMap.get(CRServo.class,"rightP");
+        s2.setDirection(DcMotorSimple.Direction.REVERSE);
         g2=linearOpMode.gamepad2;
 
 
     }
     public void Control() {
-        if (g2.triangle) {
-            s1.setPosition(1);
-            s2.setPosition(0);
-
-        } else if (g2.cross) {
-
-            s1.setPosition(0);
-            s2.setPosition(1);
-
-        }
+       float i = -g2.right_stick_y;
+       s2.setPower(i);
+       s1.setPower(i);
 
     }
 }
